745d48ba2d215d271722df07b21391a87c1064cf
Bahryantsev <bahryantsev@gmail.com>
2021-11-05 19:09:31 +0300

    phonefix

--- src/components/Content/components/header/Components/menu/components/PhoneNumber/index.js
+++ src/components/Content/components/header/Components/menu/components/PhoneNumber/index.js
@@ -4,7 +4,9 @@ import styles from './PhoneNumber.module.scss'
 export default class PhoneNumber extends React.Component{
     render(){
         return(
-            <a className = {styles.phone}href = "callto:{this.props.value}">{this.props.value}</a>
+            <a className={styles.phone} href={"callto: " + this.props.value}>
+                {this.props.value}
+            </a>
         )
     }
 }
